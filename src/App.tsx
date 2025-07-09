3fc9758ab9f75b8b1bf99eb648e6b2479edee915
programandoconro <programandoconro@gmail.com>
2025-07-09 22:42:11 +0900

    basic table with mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,42 @@
 //import { useState } from "react";
-import "./App.css";
+import { useState } from "react";
+import useSWR from "swr";
 import { Table } from "./components/table";
+import { type SearchParams, requestRepositories } from "./utils/requests";
+import "./App.css";
+import { CssBaseline } from "@mui/material";
 
 function App() {
+  const [searchParams, setSearchParams] = useState<SearchParams>({
+    query: "",
+  });
+  const [queryInput, setQueryInput] = useState("");
+
+  const { data, error, isLoading } = useSWR(searchParams, () =>
+    requestRepositories(searchParams)
+  );
+
+  const handleSearchParamsChange = <K extends keyof SearchParams>(
+    field: K,
+    value: SearchParams[K]
+  ) => {
+    setSearchParams((prev) => ({ ...prev, [field]: value }));
+  };
+
+  const handleQueryInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQueryInput(e.target.value);
+  };
+
+  const handleClick = () => {
+    handleSearchParamsChange("query", queryInput);
+  };
   return (
-    <>
+    <CssBaseline>
       <h1>Repo Searcher</h1>
-      <Table />
-    </>
+      <input value={queryInput} onChange={handleQueryInput} />
+      <button onClick={handleClick}>Search</button>
+      <Table items={data?.data.items} isLoading={isLoading} error={error} />
+    </CssBaseline>
   );
 }
 
--- src/components/table/index.tsx
+++ src/components/table/index.tsx
@@ -1,50 +1,80 @@
-import { useEffect, useState } from "react";
-import { requestRepositories, type SearchParams } from "../../utils/requests";
-import useSWR from "swr";
+import {
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+  Table as MuiTable,
+  Link,
+} from "@mui/material";
+import "./index.css";
+import type { RepoSchema } from "../../model/repo-schema";
 
-export function Table() {
-  const [searchParams, setSearchParams] = useState<SearchParams>({
-    query: "",
-  });
-  const [queryInput, setQueryInput] = useState("");
+type TableProps = {
+  items?: RepoSchema["items"];
+  error: boolean;
+  isLoading: boolean;
+};
 
-  const { data, error, isLoading } = useSWR(searchParams, () =>
-    requestRepositories(searchParams)
-  );
-
-  const handleSearchParamsChange = <K extends keyof SearchParams>(
-    field: K,
-    value: SearchParams[K]
-  ) => {
-    setSearchParams((prev) => ({ ...prev, [field]: value }));
-  };
-  const handleQueryInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setQueryInput(e.target.value);
-  };
-
-  useEffect(() => {
-    const handler = setTimeout(() => {
-      handleSearchParamsChange("query", queryInput);
-    }, 500);
-    return () => clearTimeout(handler);
-  }, [queryInput]);
-
+export function Table({ items, error, isLoading }: TableProps) {
+  if (error) return <div>There was an error</div>;
+  if (!items) return <div>No items</div>;
+  if (isLoading) return "Spinner";
   return (
-    <div>
-      <input onChange={handleQueryInput} />
-      <table>
-        <th>
-          <tr>url</tr>
-        </th>
-        <tr>
-          {error && "There was an error"}
-          {isLoading && "Loading ..."}
-        </tr>
-
-        {data?.data.items.map((item, index) => {
-          return <tr key={index}>{item.url}</tr>;
-        })}
-      </table>
-    </div>
+    <TableContainer>
+      <MuiTable>
+        <TableHead>
+          <TableRow>
+            <TableCell>
+              <Typography>Avatar</Typography>
+            </TableCell>
+            <TableCell>
+              <Typography>Repo Name</Typography>
+            </TableCell>
+            <TableCell>
+              <Typography>Description</Typography>
+            </TableCell>
+            <TableCell>
+              <Typography>Language</Typography>
+            </TableCell>
+            <TableCell>
+              <Typography>Stars</Typography>
+            </TableCell>
+            <TableCell>
+              <Typography>Last Updated</Typography>
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {items.map((item, index) => {
+            return (
+              <TableRow key={index}>
+                <TableCell>
+                  <img src={item.owner?.avatar_url} width="50px" />
+                </TableCell>
+                <TableCell>
+                  <Link target="_blank" rel="noopener" href={item.html_url}>
+                    <Typography>{item.full_name}</Typography>
+                  </Link>
+                </TableCell>
+                <TableCell>
+                  <Typography>{item.description}</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography>{item.language}</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography>{item.stargazers_count}</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography>{item.updated_at}</Typography>
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </MuiTable>
+    </TableContainer>
   );
 }
